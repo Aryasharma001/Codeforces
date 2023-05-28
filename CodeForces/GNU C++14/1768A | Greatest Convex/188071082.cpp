f2cf1fb0070a4394d9aab90806c9cdf9609ef6af
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-05-28 23:14:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1768A | Greatest Convex/188071082.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ // your code goes here
+ int t;
+ cin>>t;
+ while(t--){
+     long long int k;
+     cin>>k;
+     cout<<k-1<<endl;
+ }
+ return 0;
+}
