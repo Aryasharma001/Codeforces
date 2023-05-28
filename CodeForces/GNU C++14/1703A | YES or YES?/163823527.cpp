1e850ee5843cac787ff8f55a4826e2b800c54eed
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-05-28 23:15:42 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1703A | YES or YES?/163823527.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        
+        if(s=="YES"||s=="yes"||s=="Yes"||s=="YEs"||s=="yEs"||s=="YeS"||s=="yeS"||s=="yES"){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+ 
+    return 0;
+}
+ 
